1dcc4f324775f0cf4d13925888dc7fddba5f07bc
kokobd <contact@zelinf.net>
2021-11-22 23:53:02 +0800

    add empty solution to 540

---
+++ src/540.single-element-in-a-sorted-array.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=540 lang=rust
+ *
+ * [540] Single Element in a Sorted Array
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn single_non_duplicate(nums: Vec<i32>) -> i32 {
+        
+    }
+}
+// @lc code=end
+
